8b493a4ff708f5fd09eefd09278dd51f4cbd2b14
Osver Fernandez <osverdev@icloud.com>
2022-07-12 11:03:10 -0600

    - Create 50 Dynamic cards. - SF number image in each. - Different Color each.

--- 50CardsChallange/ContentView.swift
+++ 50CardsChallange/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            TabView{
+                ForEach(0...49,id: \.self){ index in
+                    
+                    var col1 = Double.random(in: 0...1)
+                    var col2 = Double.random(in: 0...1)
+                    var col3 = Double.random(in: 0...1)
+                    Rectangle()
+                        .foregroundColor(Color.init(.sRGB, red: col1, green: col2, blue: col3, opacity: 1))
+                        .overlay(
+                    Image(systemName: "\(index+1).circle")
+                        .resizable()
+                        .scaledToFit())
+                }
+                    
+                
+            }.frame(width: UIScreen.main.bounds.width/10*9, height: UIScreen.main.bounds.height/10*8, alignment: .center)
+                .cornerRadius(20)
+        }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .always)) // Makes tabview a swipable element.
+        
     }
 }
 
