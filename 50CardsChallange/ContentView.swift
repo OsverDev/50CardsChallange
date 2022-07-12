172011a2a12426a579ada25ea462d0f3d6dac539
Osver Fernandez <osverdev@icloud.com>
2022-07-12 10:18:37 -0600

    Initial Commit

---
+++ 50CardsChallange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  50CardsChallange
+//
+//  Created by Osver Fernandez on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 50CardsChallange/_0CardsChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0CardsChallangeApp.swift
+//  50CardsChallange
+//
+//  Created by Osver Fernandez on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct _0CardsChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
